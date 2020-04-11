62a9d2248a3441091579639f0990af27a39b8594
SHOBHIT <shobhitsaini95@gmail.com>
2020-04-11 19:23:15 +0530

    Create 1021.Remove_Outermost_Parentheses.cpp

---
+++ 1021.Remove_Outermost_Parentheses.cpp
@@ -0,0 +1,71 @@
+/*
+A valid parentheses string is either empty (""), "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.  For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+
+A valid parentheses string S is primitive if it is nonempty, and there does not exist a way to split it into S = A+B, with A and B nonempty valid parentheses strings.
+
+Given a valid parentheses string S, consider its primitive decomposition: S = P_1 + P_2 + ... + P_k, where P_i are primitive valid parentheses strings.
+
+Return S after removing the outermost parentheses of every primitive string in the primitive decomposition of S.
+
+ 
+
+Example 1:
+
+Input: "(()())(())"
+Output: "()()()"
+Explanation: 
+The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
+After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
+Example 2:
+
+Input: "(()())(())(()(()))"
+Output: "()()()()(())"
+Explanation: 
+The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
+After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())".
+Example 3:
+
+Input: "()()"
+Output: ""
+Explanation: 
+The input string is "()()", with primitive decomposition "()" + "()".
+After removing outer parentheses of each part, this is "" + "" = "".
+ 
+
+Note:
+
+S.length <= 10000
+S[i] is "(" or ")"
+S is a valid parentheses string
+
+*/
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        
+        int size = s.size() , i , j = 1 , counter = 0 ;
+        string str = "" ;
+        vector<int> v;
+        for( i = 0 ; i < size ; i++ )
+        {
+            if( s[i] == '(' )
+                counter++;
+            else if( s[i] == ')' )
+                counter--;
+            if(counter == 0)
+                v.push_back(i) ;
+        }
+        
+        int k = 0 ;
+        for( i = 0 ; i < size && k < v.size() ; )
+        {
+            for( j = i + 1 ; j < v[k] ; j++ )
+            {
+                str += s[j] ;
+            }
+            i = j + 1;
+            k++ ;
+        }
+        return str ;
+    }
+};
