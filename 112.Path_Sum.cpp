23cb250f7418b42b7573a4fd0ba28cf78e1f7c2b
SHOBHIT <shobhitsaini95@gmail.com>
2020-04-14 11:07:15 +0530

    Update 112.Path_Sum.cpp

--- 112.Path_Sum.cpp
+++ 112.Path_Sum.cpp
@@ -27,36 +27,30 @@ return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
  */
 class Solution {
 public:
-    int var = 0 , flag = 0 ;
-    void fun ( TreeNode* root , int sum )
-    {
-        var += root -> val ; 
-        if( root -> left == NULL && root -> right == NULL )
-        {
-            if( var == sum )
-            {
-                flag = 1;
-            }
-            var -= root -> val ;
-            return ;
-        }
-        if( root -> left != NULL )
-        {
-            fun( root -> left , sum ) ;
-        }
-        if( root -> right != NULL )
-        {
-            fun( root -> right , sum ) ; 
-        }
-        var -= root -> val ;
-    }
     bool hasPathSum(TreeNode* root, int sum) {
-        
         if( root == NULL )
-            return 0 ;
-        fun ( root , sum ) ;
-        if( flag == 1 )
+            return false;
+        if( root -> left == NULL && root -> right == NULL && sum - root -> val == 0 )
             return true ;
-        return false ;
+return (hasPathSum( root -> left , sum - root -> val ) || hasPathSum( root -> right , sum - root -> val )) ;
+    }
+};
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        if( root == NULL )
+            return false;
+        if( root -> left == NULL && root -> right == NULL && sum - root -> val == 0 )
+            return true ;
+return (hasPathSum( root -> left , sum - root -> val ) || hasPathSum( root -> right , sum - root -> val )) ;
     }
 };
