f95e2a0b1902962349114304d103bcfc7c3e0e81
SHOBHIT <shobhitsaini95@gmail.com>
2020-04-18 16:23:20 +0530

    Update 1325.Delete_Leaves_With_a_Given_Value.cpp

--- 1325.Delete_Leaves_With_a_Given_Value.cpp
+++ 1325.Delete_Leaves_With_a_Given_Value.cpp
@@ -100,3 +100,10 @@ public:
         return root ;
     }
 };
+/* recursive approach 
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        
+        if( root -> left ) root -> left = removeLeafNodes( root -> left , target ) ;
+        if( root -> right ) root -> right = removeLeafNodes( root -> right , target ) ;
+        return root -> left == root -> right && root -> val == target ? NULL : root ; 
+        }
