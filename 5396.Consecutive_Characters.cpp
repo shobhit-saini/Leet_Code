9a6713b1f132fa63ef966f5d76b9218cacf0311e
SHOBHIT <shobhitsaini95@gmail.com>
2020-05-16 21:35:37 +0530

    Create 5396.Consecutive_Characters.cpp

---
+++ 5396.Consecutive_Characters.cpp
@@ -0,0 +1,55 @@
+/*
+Given a string s, the power of the string is the maximum length of a non-empty substring that contains only one unique character.
+
+Return the power of the string.
+
+ 
+
+Example 1:
+
+Input: s = "leetcode"
+Output: 2
+Explanation: The substring "ee" is of length 2 with the character 'e' only.
+Example 2:
+
+Input: s = "abbcccddddeeeeedcba"
+Output: 5
+Explanation: The substring "eeeee" is of length 5 with the character 'e' only.
+Example 3:
+
+Input: s = "triplepillooooow"
+Output: 5
+Example 4:
+
+Input: s = "hooraaaaaaaaaaay"
+Output: 11
+Example 5:
+
+Input: s = "tourist"
+Output: 1
+ 
+
+Constraints:
+
+1 <= s.length <= 500
+s contains only lowercase English letters.
+*/
+class Solution {
+public:
+    int maxPower(string s) {
+        
+        int max = INT_MIN, i, size = s.size(), count = 1;
+        for( i = 1; i < size; i++ )
+        {
+            if( s[i] == s[i-1] )
+                count++;
+            else
+            {
+                if( max < count )
+                    max = count;
+                count = 1;
+            }
+        }
+        return count > max ? count : max;
+    }
+};
