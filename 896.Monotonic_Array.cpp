ec6d3339b70daef5476a3a8cdec9422960414340
SHOBHIT <shobhitsaini95@gmail.com>
2020-05-22 21:02:37 +0530

    Create 896.Monotonic_Array.cpp

---
+++ 896.Monotonic_Array.cpp
@@ -0,0 +1,60 @@
+/*
+An array is monotonic if it is either monotone increasing or monotone decreasing.
+
+An array A is monotone increasing if for all i <= j, A[i] <= A[j].  An array A is monotone decreasing if for all i <= j, A[i] >= A[j].
+
+Return true if and only if the given array A is monotonic.
+
+ 
+
+Example 1:
+
+Input: [1,2,2,3]
+Output: true
+Example 2:
+
+Input: [6,5,4,4]
+Output: true
+Example 3:
+
+Input: [1,3,2]
+Output: false
+Example 4:
+
+Input: [1,2,4,5]
+Output: true
+Example 5:
+
+Input: [1,1,1]
+Output: true
+ 
+
+Note:
+
+1 <= A.length <= 50000
+-100000 <= A[i] <= 100000
+*/
+class Solution {
+public:
+    bool isMonotonic(vector<int>& A) {
+        
+        int size = A.size(), i, flag1 = 0, flag2 = 0;
+        for( i = 0; i < size-1; i++ )
+        {
+            if(flag1 == 0 && A[i] < A[i+1])
+            {
+                flag2 = 1;
+                continue;
+            }
+            if(flag2 == 0 && A[i] > A[i+1])
+            {
+                flag1 = 1;
+                continue;
+            }
+            if( i+1 < size && A[i] == A[i+1] )
+                continue;
+            return false;
+        }
+        return true;
+    }
+};
