5f4e50e46489b49dc97e3cdad5c059c1d63485c4
SHOBHIT <shobhitsaini95@gmail.com>
2020-05-30 21:33:31 +0530

    Create 1460.Make_Two_Arrays_Equal_by_Reversing_Sub-arrays.cpp

---
+++ 1460.Make_Two_Arrays_Equal_by_Reversing_Sub-arrays.cpp
@@ -0,0 +1,69 @@
+/*
+Given two integer arrays of equal length target and arr.
+
+In one step, you can select any non-empty sub-array of arr and reverse it. You are allowed to make any number of steps.
+
+Return True if you can make arr equal to target, or False otherwise.
+
+ 
+
+Example 1:
+
+Input: target = [1,2,3,4], arr = [2,4,1,3]
+Output: true
+Explanation: You can follow the next steps to convert arr to target:
+1- Reverse sub-array [2,4,1], arr becomes [1,4,2,3]
+2- Reverse sub-array [4,2], arr becomes [1,2,4,3]
+3- Reverse sub-array [4,3], arr becomes [1,2,3,4]
+There are multiple ways to convert arr to target, this is not the only way to do so.
+Example 2:
+
+Input: target = [7], arr = [7]
+Output: true
+Explanation: arr is equal to target without any reverses.
+Example 3:
+
+Input: target = [1,12], arr = [12,1]
+Output: true
+Example 4:
+
+Input: target = [3,7,9], arr = [3,7,11]
+Output: false
+Explanation: arr doesn't have value 9 and it can never be converted to target.
+Example 5:
+
+Input: target = [1,1,1,1,1], arr = [1,1,1,1,1]
+Output: true
+ 
+
+Constraints:
+
+target.length == arr.length
+1 <= target.length <= 1000
+1 <= target[i] <= 1000
+1 <= arr[i] <= 1000
+
+
+*/
+class Solution {
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        
+        multimap<int, int>mp1,mp2;
+        int i, size1 = target.size(), size2 = arr.size();
+        if( size1 != size2 )
+            return false;
+        for( i = 0; i < size1; i++ )
+        {
+            mp1.insert({target[i], 1});
+            mp2.insert({arr[i], 1});
+        }
+        multimap<int, int>:: iterator itr1, itr2;
+        for( itr1 = mp1.begin(), itr2 = mp2.begin(); itr1 != mp1.end(), itr2 != mp2.end(); itr1++, itr2++ )
+        {
+            if( itr1->first != itr2 -> first || itr1->second != itr2 ->second )
+                return false;
+        }
+        return true;
+    }
+};
