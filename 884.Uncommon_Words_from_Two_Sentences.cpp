a96f1885d0fddcc4297b9725f576f54eedbc5250
SHOBHIT <shobhitsaini95@gmail.com>
2020-06-18 23:59:47 +0530

    Update 884.Uncommon_Words_from_Two_Sentences.cpp

--- 884.Uncommon_Words_from_Two_Sentences.cpp
+++ 884.Uncommon_Words_from_Two_Sentences.cpp
@@ -25,3 +25,59 @@ Note:
 0 <= B.length <= 200
 A and B both contain only spaces and lowercase letters.
 */
+class Solution {
+public:
+    vector<string> uncommonFromSentences(string A, string B) {
+        
+        map<string, int>mp;
+        string str = "";
+        int i, size_A = A.size(), size_B = B.size();
+        vector<string>res;
+        for( i = 0; i < size_A; i++ )
+        {
+            if( i == size_A-1 )
+            {
+                str += A[i];
+                mp[str]++;
+                break;
+            }
+            if( A[i] == ' ' )
+            {
+                mp[str]++;
+                str = "";
+            }
+            else
+            {
+                str += A[i];
+            }
+        }
+        
+        str = "";
+        for( i = 0; i < size_B; i++ )
+        {
+            if( i == size_B-1 )
+            {
+                str += B[i];
+                mp[str]++;
+                break;
+            }
+            if( B[i] == ' ' )
+            {
+                mp[str]++;
+                str = "";
+            }
+            else
+            {
+                str += B[i];
+            }
+        }
+        map<string, int>:: iterator itr;
+        
+        for( itr = mp.begin(); itr != mp.end(); itr++ )
+        {
+            if( itr->second == 1 )
+                res.push_back(itr->first);
+        }
+        return res;
+    }
+};
