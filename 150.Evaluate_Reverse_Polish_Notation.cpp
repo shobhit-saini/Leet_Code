5ff925e74539200678c88133910fac55f067fee1
SHOBHIT <shobhitsaini95@gmail.com>
2020-07-11 12:42:47 +0530

    Create 150.Evaluate_Reverse_Polish_Notation.cpp

---
+++ 150.Evaluate_Reverse_Polish_Notation.cpp
@@ -0,0 +1,66 @@
+/*
+Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+
+Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+
+Note:
+
+Division between two integers should truncate toward zero.
+The given RPN expression is always valid. That means the expression would always evaluate to a result and there won't be any divide by zero operation.
+Example 1:
+
+Input: ["2", "1", "+", "3", "*"]
+Output: 9
+Explanation: ((2 + 1) * 3) = 9
+Example 2:
+
+Input: ["4", "13", "5", "/", "+"]
+Output: 6
+Explanation: (4 + (13 / 5)) = 6
+Example 3:
+
+Input: ["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"]
+Output: 22
+Explanation: 
+  ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+= ((10 * (6 / (12 * -11))) + 17) + 5
+= ((10 * (6 / -132)) + 17) + 5
+= ((10 * 0) + 17) + 5
+= (0 + 17) + 5
+= 17 + 5
+= 22
+*/
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        
+        stack<int>s;
+        int size = tokens.size(), i = 0, a, b, c;
+        while( i < size )
+        {
+            if( tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "/" || tokens[i] == "*" )
+            {
+                b = s.top();
+                s.pop();
+                a = s.top();
+                s.pop();
+                if( tokens[i] == "+" )
+                    c = a + b;
+                else if( tokens[i] == "-" )
+                    c = a-b;
+                else if( tokens[i] == "*" )
+                    c = a*b;
+                else
+                    c = a/b;
+                s.push(c);
+            }
+            else
+            {
+                int temp = stoi(tokens[i]);
+                s.push(temp);
+            }
+            i++;
+        }
+        return s.top();
+    }
+};
