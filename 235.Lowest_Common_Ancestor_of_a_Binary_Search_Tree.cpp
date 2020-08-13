94904d99b44306163ce54af40ee4ce4396da8ca9
SHOBHIT <shobhitsaini95@gmail.com>
2020-08-13 23:36:43 +0530

    Update 235.Lowest_Common_Ancestor_of_a_Binary_Search_Tree.cpp

--- 235.Lowest_Common_Ancestor_of_a_Binary_Search_Tree.cpp
+++ 235.Lowest_Common_Ancestor_of_a_Binary_Search_Tree.cpp
@@ -38,18 +38,13 @@ class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         
-        if( root == NULL )
-        {
+        if(root == NULL)
             return NULL;
-        }
-        if( ( p->val < root->val && root->val < q->val ) || ( q->val < root->val && root->val < p->val ) )
+        if(root->val > p->val && root->val > q->val)
+            return lowestCommonAncestor(root->left, p, q);
+        if(root->val < p->val && root->val < q->val)
+            return lowestCommonAncestor(root->right, p, q);
+        else
             return root;
-        else if( p->val == root->val || q->val == root->val )
-            return root;
-        else if( p->val < root->val && q->val < root->val )
-            return lowestCommonAncestor( root -> left, p, q );
-        else 
-            return lowestCommonAncestor( root -> right, p, q );
-        
     }
 };
